ebe574c3ef65e5a1bc9ead4650a519107908ec27
Danil <ferrum268@mail.ru>
2023-08-20 14:48:22 +0500

    write linter.read_all_tokens()

--- linter.py
+++ linter.py
@@ -4,9 +4,15 @@ from os import getcwd
 
 class Linter:
     def __init__(self, code_file, rules=getcwd() + "\\default_rules.json"):
-        self.tokenizer = Tokenizer(code_file)
+        self._tokenizer = Tokenizer(code_file)
         self._rules = rules
+        self._tokens_lines = []
+    
+    def read_all_tokens(self):
+        tokens_line = self._tokenizer.read_line()
+        while tokens_line:
+            self._tokens_lines.append(tokens_line)
+            tokens_line = self._tokenizer.read_line()
 
     def run(self):
-        print("Running")
-        pass
+        self.read_all_tokens()
