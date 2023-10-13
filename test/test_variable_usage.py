c55f219631a4e4d99e947248864f583b455afc2a
Danil <ferrum268@mail.ru>
2023-10-13 14:36:16 +0500

    write tests for variable_usage.py

--- test/test_variable_usage.py
+++ test/test_variable_usage.py
@@ -1,11 +1,36 @@
 import unittest
 from variable_usage import VariableUsage
+from tokenizer import Tokenizer
+from parser_class import Parser
 
 
 PATH_TO_DIR = r"D:\GitHubProjects\Linter\test\test_files"
 
 
 class Test(unittest.TestCase):
+    @staticmethod
+    def tokenizing_lines(relative_path):
+        path = PATH_TO_DIR + relative_path
+        tokenizer = Tokenizer(path)
+        result = []
+        line = tokenizer.read_line()
+        while line:
+            result.append(line)
+            line = tokenizer.read_line()
+        return result
+
     def test_all_used(self):
+        tokens_lines = self.tokenizing_lines("\\variable_usage_all_used.txt")
+        parser = Parser(tokens_lines)
+        unused_variables = VariableUsage.check_unused_variables(
+            parser.table, tokens_lines
+        )
+        self.assertTrue(len(unused_variables) == 0)
 
-
+    def test_unused_variables(self):
+        tokens_lines = self.tokenizing_lines("\\variable_usage_unused_variables.txt")
+        parser = Parser(tokens_lines)
+        unused_variables = VariableUsage.check_unused_variables(
+            parser.table, tokens_lines
+        )
+        self.assertTrue(len(unused_variables) == 1 and unused_variables[0] == "a")
--- variable_usage.py
+++ variable_usage.py
@@ -14,7 +14,8 @@ class VariableUsage:
         for line in tokens_lines:
             for i in range(len(line)):
                 VariableUsage.check_line_variables(
-                    variable_used, line
+                    variable_used,
+                    VariableUsage.skip_whitespaces_and_comments(line)
                 )
         result = [name for name in variable_used if not variable_used[name]]
         return result
@@ -26,3 +27,11 @@ class VariableUsage:
                     and line[i].value in variable_used:
                 if i + 1 < len(line) and line[i + 1].value not in ASSIGNMENTS:
                     variable_used[line[i].value] = True
+
+    @staticmethod
+    def skip_whitespaces_and_comments(tokens_line):
+        result = []
+        for token in tokens_line:
+            if token.type not in [TokenType.SPACE, TokenType.COMMENT]:
+                result.append(token)
+        return result
